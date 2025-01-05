ef2ec4e715b93fcac4dc5b6ebb444628b18c33c1
Harry Vince <harryavince@gmail.com>
2025-01-05 19:01:58 +0000

    refactor: change how final output looks in main

--- main.go
+++ main.go
@@ -1,43 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/harryvince/ds-algos/arrays"
+)
 
 func main() {
-    println("----- BUBBLE SORT -----")
-	array := []int{7, 12, 9, 11, 3}
-	fmt.Printf("Initial Array: %v\n", array)
-	result := BubbleSort(array)
-	fmt.Printf("Sorted Array: %v\n", result)
-    println("----- END OF BUBBLE SORT -----")
-}
-
-func BubbleSort(values []int) []int {
-	length := len(values) - 1
-
-	x := 0
-	noSwap := 0
-	for {
-		current := values[x]
-		next := values[x+1]
-
-		if current > next {
-			values[x+1] = current
-			values[x] = next
-		} else {
-			noSwap++
-		}
-
-		x++
-
-		if noSwap == length {
-			break
-		}
-
-		if x == length {
-			x = 0
-			noSwap = 0
-		}
-	}
-
-	return values
+	arrays.BubbleSort()
 }
